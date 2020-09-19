2d18f665eb103be10d72b68429f7eb7565b63e5a
Autumn Fotopoulos <acarte94@gmail.com>
2020-09-19 11:23:18 -0500

    inclass work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,15 @@
 import '../styles/main.scss';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import authData from './helpers/data/authData';
+import auth from './components/auth/auth';
+import myNavBar from './components/myNavbar/myNavbar';
 
 const init = () => {
-  console.warn('HELLO!');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  myNavBar.logoutEvent();
 };
 
 init();
